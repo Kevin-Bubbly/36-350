da6e17e455ce0816c35a28113840a030336f4726
Kevin Gu <kevin.yutaogu@gmail.com>
2020-07-31 05:47:47 -0400

    this is for Q 17

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,1 +1,9 @@
-cat("whats up dawgs")
+a="We are human beings."
+b="Who are you?"
+c="1 2 3 4 5 6 7 8  9  10
+----------------------
+0 0 0 1 1 1 1 00 00 00
+0 1 1 0 0 1 1 00 00 10
+1 0 1 0 1 0 1 01 11 01
+X X X X X X X X  X  X "
+cat(a,"\n",b,"\n",c)
